751457c156240aa519fb1368ce55f70346b31b1c
nhlutterodt <nhlutterodt@gmail.com>
2017-01-25 12:55:06 -0500

    Update server.js
    This version is simply to display the form.

--- server.js
+++ server.js
@@ -1,20 +1,9 @@
 var http = require('http');
 var fs = require('fs');
-var formidable = require("formidable");
-var util = require('util');
-
-
-//create a server for the application
 var server = http.createServer(function (req, res) {
-    if (req.method.toLowerCase() == 'get') {
-        displayForm(res);
-    } else if (req.method.toLowerCase() == 'post') {
-        //processAllFieldsOfTheForm(req, res);
-        processFormFieldsIndividual(req, res);
-    }
+    displayForm(res);
 });
 
-//display the index html page that has the form
 function displayForm(res) {
     fs.readFile('index.html', function (err, data) {
         res.writeHead(200, {
@@ -26,8 +15,5 @@ function displayForm(res) {
     });
 }
 
-
-
-//end of salesforce login block
 server.listen(1185);
 console.log("server listening on 1185");
