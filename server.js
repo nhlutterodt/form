f7f23a2b69a09a6814b0e2e9ff377e4912105319
nhlutterodt <nhlutterodt@gmail.com>
2017-01-25 12:52:05 -0500

    Update server.js
    This version of the server.js file is supposed to simply show the html form.

--- server.js
+++ server.js
@@ -27,67 +27,6 @@ function displayForm(res) {
 }
 
 
-//using formidable to parse the form in the HTML page
-function processAllFieldsOfTheForm(req, res) {
-    var form = new formidable.IncomingForm();
-
-    form.parse(req, function (err, fields, files) {
-        //Store the data from the fields in your data store.
-        //The data store could be a file or database or any other store based
-        //on your application.
-        res.writeHead(200, {
-            'content-type': 'text/plain'
-        });
-        res.write('received the data:\n\n');
-        res.end(util.inspect({
-            fields: fields,
-            files: files
-        }));
-    });
-}
-
-function processFormFieldsIndividual(req, res) {
-    //Store the data from the fields in your data store.
-    //The data store could be a file or database or any other store based
-    //on your application.
-    var fields = [];
-    var form = new formidable.IncomingForm();
-    form.on('field', function (field, value) {
-        console.log(field);
-        console.log(value);
-        fields[field] = value;
-    });
-
-    form.on('end', function () {
-        res.writeHead(200, {
-            'content-type': 'text/plain'
-        });
-        res.write('received the data:\n\n');
-        res.end(util.inspect({
-            fields: fields
-        }));
-    });
-    form.parse(req);
-}
-
-
-//salesforce login block
-var sf = require('node-salesforce');
-var conn = new sf.Connection({
-  // you can change loginUrl to connect to sandbox or prerelease env. 
-  loginUrl : 'https://login.salesforce.com' 
-});
-conn.login(nhlutterodt@hillel.org.trailhead, Deadrise1!, function(err, userInfo) {
-  if (err) { return console.error(err); }
-  // Now you can get the access token and instance URL information. 
-  // Save them to establish connection next time. 
-  console.log(conn.accessToken);
-  console.log(conn.instanceUrl);
-  // logged in user property 
-  console.log("User ID: " + userInfo.id);
-  console.log("Org ID: " + userInfo.organizationId);
-  // ... 
-});
 
 //end of salesforce login block
 server.listen(1185);
